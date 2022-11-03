5d076d143464b8fbe324513fbfefd41f2fde6092
mgn1312 <114455110+mgn1312@users.noreply.github.com>
2022-11-03 11:39:13 +0900

    Add files via upload

--- collector/collector_naver_movie_review.py
+++ collector/collector_naver_movie_review.py
@@ -1,7 +1,7 @@
 import requests
-
+import re
 from bs4 import BeautifulSoup
-
+import math
 
 
 ####################
@@ -26,9 +26,35 @@ def movie_title_crawler(movie_code):
 # 리뷰 수집 (리뷰, 평점, 작성자 ,작성 일자) + 제목
 def movie_review_crawler(movie_code):
     title = movie_title_crawler(movie_code)  # 제목 수집
-    # 리뷰를 수집하는 코드 작성
-    url = ''
-
-    print(f'제목: {title}')
+    print(f'>> Start collecting movies for {title}')
 
     # set {제목, 리뷰, 평점, 작성자, 작성일자}
+    url = f'https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code={movie_code}&type=after&onlyActualPointYn=N&onlySpoilerPointYn=N&order=newest&page=1'
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    all_count = doc.select('strong.total > em')[0].get_text()  # 리뷰 전체 수
+    # print(type(all_count))
+    # "2480"  -> 2480 (o)
+    # "2,480" -> 2480 (x) 문자포함 변환 불가능
+
+    # 1. 숫자만 추출 : 정규식
+    numbers = re.sub(r'[^0-9]', '', all_count)
+    pages = math.ceil(int(numbers) / 10)
+    print(f'The total number of pages to collect is {pages}')
+
+    # 해당 페이지 리뷰 수집!
+    count = 0  # 전체 리뷰 수를 count
+
+    for page in range(1, pages + 1):
+        url = f'https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code={movie_code}&type=after&onlyActualPointYn=N&onlySpoilerPointYn=N&order=newest&page={page}'
+        result = requests.get(url)
+        doc = BeautifulSoup(result.text, 'html.parser')
+        review_list = doc.select('div.score_result > ul > li')  # 한 페이지의 리뷰 10건
+
+        for i, one in enumerate(review_list):
+            # 리뷰, 평점, 작성자, 작성일자
+            review = one.select('div.score_reple > p > span')[-1].get_text().strip()
+            score = one.select('div.star_score > em')[0].get_text()
+            print(f'# Review : {review}')
+            print(f'# Score : {score}')
+        break
